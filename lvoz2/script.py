ae6f710aa5ecab21b6d4fe88889782bc0ebe023f
lv_oz2 <65639555+lvoz2@users.noreply.github.com>
2021-05-31 13:11:12 +1000

    Update script.py

--- lvoz2/script.py
+++ lvoz2/script.py
@@ -10,33 +10,33 @@ def main():
 				r = "0"
 			if x == 1:
 				r = "1"
-		    	if x == 2:
+			if x == 2:
 				r = "2"
-		    	if x == 3:
+			if x == 3:
 				r = "3"
-		    	if x == 4:
+			if x == 4:
 				r = "4"
-		    	if x == 5:
+			if x == 5:
 				r = "5"
-		    	if x == 6:
+			if x == 6:
 				r = "6"
-		    	if x == 7:
+			if x == 7:
 				r = "7"
-		    	if x == 8:
+			if x == 8:
 				r = "8"
-		    	if x == 9:
+			if x == 9:
 				r = "9"
-		    	if x == 10:
+			if x == 10:
 				r = "A"
-		    	if x == 11:
+			if x == 11:
 				r = "B"
-		    	if x == 12:
+			if x == 12:
 				r = "C"
-		    	if x == 13:
+			if x == 13:
 				r = "D"
-		    	if x == 14:
+			if x == 14:
 				r = "E"
-		    	if x == 15:
+			if x == 15:
 				r = "F"
 			colour = colour + r
 		document["output"].style.backgroundColor = colour
