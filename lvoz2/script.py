68a27d6ba5d89bf3dde28ced742ebae9f8dc09f6
lv_oz2 <65639555+lvoz2@users.noreply.github.com>
2021-07-14 11:00:13 +1000

    Update script.py

--- lvoz2/script.py
+++ lvoz2/script.py
@@ -1,5 +1,21 @@
-import turtle
-t = turtle.Turtle()
-t.forward(10)
 print("Hello, Python has worked for once!")
 print("We are all happy!!")
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.neural_network import BernoulliRBM
+X = np.array([[0, 0, 0], [0, 1, 1], [1, 0, 1], [1, 1, 1]])
+model = BernoulliRBM(n_components=2)
+model.fit(X)
+# Plotting
+
+plt.figure(figsize=(4.2, 4))
+for i, comp in enumerate(model.components_):
+    plt.subplot(10, 10, i + 1)
+    plt.imshow(comp.reshape((8, 8)), cmap=plt.cm.gray_r,
+               interpolation='nearest')
+    plt.xticks(())
+    plt.yticks(())
+plt.suptitle('100 components extracted by RBM', fontsize=16)
+plt.subplots_adjust(0.08, 0.02, 0.92, 0.85, 0.08, 0.23)
+
+plt.show()
